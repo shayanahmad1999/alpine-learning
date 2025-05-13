21e142d34612cfe47afef29ef66df8d60b028622
ShayanArchiwiz <shayan.ahmad@archiwiz.com>
2025-05-13 17:00:35 +0500

    lesson 4 updated and used some mutted properties

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,11 +27,19 @@
     </div>
 
     <div x-data="game()" class="px-10 flex items-center justify-center min-h-screen">
+        <h1 class="fixed top-0 right-0 p-10 font-bold text-3xl">
+            <span x-text="points"></span>
+            <span class="text-xs">pts</span>
+        </h1>
         <div class="flex-1 grid grid-cols-4 gap-10">
             <template x-for="card in cards">
-                <div :style="'background: ' + (card.flipped ? card.color : '#999')" class="h-32 cursor-pointer"
-                    @click="card.flipped = ! card.flipped">
-
+                <div>
+                    <button
+                        x-show="! card.cleared"
+                        :style="'background: ' + (card.flipped ? card.color : '#999')" 
+                        class="w-full h-32"
+                        @click="flipCard(card)"
+                    ></button>
                 </div>
             </template>
         </div>
@@ -50,6 +58,41 @@
                     {color: 'blue',flipped: false, cleared: false },
                     {color: 'yellow',flipped: false, cleared: false },
                 ],
+
+                get flippedCards() {
+                    return this.cards.filter(card => card.flipped);
+                },
+
+                get clearedCards() {
+                 return this.cards.filter(card => card.cleared);  
+                },
+
+                get remainingCards() {
+                 return this.cards.filter(card => ! card.cleared);  
+                },
+
+                get points() {
+                    return this.clearedCards.length; 
+                },
+
+                flipCard(card){
+                    card.flipped = ! card.flipped;
+
+                    if(this.flippedCards.length === 2) {
+                        if(this.hasMatch()) {
+                            this.flippedCards.forEach(card => card.cleared = true);
+                            if(! this.remainingCards.length){
+                                alert('you won');
+                            }
+                        }
+
+                        this.flippedCards.forEach(card => card.flipped = false);
+                    }
+                },
+
+                hasMatch() {
+                    return this.flippedCards[0]['color'] === this.flippedCards[1]['color'];
+                },
             };
         }
     </script>
