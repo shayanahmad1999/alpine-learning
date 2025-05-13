be75af5b83f04a424914278681825760578de711
ShayanArchiwiz <shayan.ahmad@archiwiz.com>
2025-05-13 18:11:53 +0500

    toggle visibilty using state

--- resources/views/learning.blade.php
+++ resources/views/learning.blade.php
@@ -7,6 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <style>
+        .active {
+            background: blue;
+            color: white;
+        }
+    </style>
 </head>
 
 <body>
@@ -24,6 +30,37 @@
     <div x-data="{ first: 0, second: 0 }">
         <input type="text" x-model.number="first"> + <input type="text" x-model.number="second"> = <output
             x-text="first + second"></output>
+    </div>
+
+    <br>
+    <br>
+    <br>
+
+    <div x-data="{ show: false }">
+        <h1 x-show="show">Hello World</h1>
+        <button @click="show = ! show" x-text="show ? 'Hide' : 'Show'"></button>
+    </div>
+
+    <br>
+    <br>
+    <br>
+
+    <div x-data="{ currentTab: 'first' }">
+        <button @click="currentTab = 'first'" :class="{ 'active': currentTab === 'first' }">First</button>
+        <button @click="currentTab = 'second'" :class="{ 'active': currentTab === 'second' }">Second</button>
+        <button @click="currentTab = 'third'" :class="{ 'active': currentTab === 'third' }">Third</button>
+
+        <div style="border: 1px dotted gray; padding: 1rem; margin-top: 1rem;">
+            <div x-show="currentTab === 'first'">
+                <p>First Tab.</p>
+            </div>
+            <div x-show="currentTab === 'second'">
+                <p>Second Tab.</p>
+            </div>
+            <div x-show="currentTab === 'third'">
+                <p>Third Tab.</p>
+            </div>
+        </div>
     </div>
 </body>
 
